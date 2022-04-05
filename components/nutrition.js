576de3154b18e4a5ad767fa137e52e6c572ee4c6
Shunya Shishido <arerreee@gmail.com>
2022-04-06 08:48:13 +0900

    Fix spelling

--- components/nutrition.js
+++ components/nutrition.js
@@ -20,7 +20,7 @@ export default function Nutrition(props) {
     <div className='bg-white shadow overflow-hidden sm:rounded-lg mt-8'>
       <div className='px-4 py-5 sm:px-6'>
         <h3 className='text-lg leading-6 font-medium text-gray-900'>Nutrition Facts</h3>
-        <p className='mt-1 max-w-2xl text-sm text-gray-500'>amount per {amountPer}</p>
+        <p className='mt-1 max-w-2xl text-sm text-gray-500'>Amount per {amountPer}</p>
       </div>
       <div className='border-t border-gray-200'>
         <dl>
